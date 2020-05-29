7d913157befc38c9843617fb3a0ce3b8f0a92417
SSripilaipong <santhapon.s@siametrics.com>
2020-05-30 02:48:18 +0700

    add attributes inheritance

--- otscrape/base/data_model.py
+++ otscrape/base/data_model.py
@@ -3,6 +3,7 @@ from functools import cached_property
 
 class DataModelMeta(type):
     def __new__(mcs, name, bases, dct):
+        print(dct)
         attrs = {}
 
         for name, obj in dct.items():
@@ -10,6 +11,12 @@ class DataModelMeta(type):
                 attrs[name] = obj
             elif getattr(obj, 'is_extractor', None):
                 attrs[name] = attribute(obj.extract)
+
+        for base in bases:
+            if not hasattr(base, '_attributes'):
+                continue
+            for name in getattr(base, '_attributes'):
+                attrs[name] = getattr(base, name)
 
         dct.update(attrs)
         dct['_attributes'] = set(attrs.keys())
