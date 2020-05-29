5633627a1451d60ace665a956ca6c28665af0a87
SSripilaipong <santhapon.s@siametrics.com>
2020-05-30 02:59:24 +0700

    remove load() parameters

--- otscrape/core/io/request/loader.py
+++ otscrape/core/io/request/loader.py
@@ -3,34 +3,33 @@ from otscrape.base.loader import Loader
 
 
 class RequestLoader(Loader):
-    def __init__(self, method, default_kwargs=None, accept_status_codes=(200,)):
+    def __init__(self, url, method, kwargs=None, accept_status_codes=(200,)):
+        self.url = url
         self.method = method
-        self.default_kwargs = default_kwargs or {}
+        self.kwargs = kwargs or {}
         self.accept_status_codes = accept_status_codes
 
-    def make_request(self, url, **kwargs):
-        _kwargs = dict(**self.default_kwargs, **kwargs)
-
-        resp = requests.request(self.method, url, **_kwargs)
+    def make_request(self):
+        resp = requests.request(self.method, self.url, **self.kwargs)
         if resp.status_code in self.accept_status_codes:
             return resp
         resp.raise_for_status()
 
-    def load(self, url, **kwargs):
-        self.on_requesting(url, **kwargs)
+    def load(self):
+        self.on_requesting()
 
         try:
-            return self.make_request(url, **kwargs)
+            return self.make_request()
         except requests.RequestException as e:
-            return self.on_request_error(e, url, **kwargs)
+            return self.on_request_error(e)
         finally:
-            self.on_requested(url, **kwargs)
+            self.on_requested()
 
-    def on_requesting(self, url, **kwargs):
+    def on_requesting(self):
         pass
 
-    def on_requested(self, url, **kwargs):
+    def on_requested(self):
         pass
 
-    def on_request_error(self, exception, url, **kwargs):
+    def on_request_error(self, exception):
         raise exception
