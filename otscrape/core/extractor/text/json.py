f6f6ac4f9128b9f227fbc929fae1e5ca4a381bcb
SSripilaipong <santhapon.s@siametrics.com>
2020-11-14 03:29:42 +0700

    remove deprecated encoding parameter in json.loads

--- otscrape/core/extractor/text/json.py
+++ otscrape/core/extractor/text/json.py
@@ -4,14 +4,13 @@ from otscrape.core.base.extractor import Extractor
 
 
 class JSONDict(Extractor):
-    def __init__(self, target=None, *, encoding=None, project=True, replace_error=None, **kwargs):
+    def __init__(self, target=None, *, project=True, replace_error=None, **kwargs):
         super().__init__(target=target, project=project, replace_error=replace_error)
 
-        self.encoding = encoding
         self.kwargs = kwargs
 
     def extract(self, page, cache):
         target = self.target
         assert isinstance(page[target], (str, bytes))
 
-        return json.loads(page[target], encoding=self.encoding, **self.kwargs)
+        return json.loads(page[target], **self.kwargs)
