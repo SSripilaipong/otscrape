23570d71e6d62e0f60639ef6d9fb2ab2c4f34558
SSripilaipong <santhapon.s@siametrics.com>
2020-11-29 20:04:42 +0700

    refactor Map, MapStar: inherit from MapBase

--- otscrape/core/extractor/map.py
+++ otscrape/core/extractor/map.py
@@ -4,41 +4,39 @@ from otscrape.core.base.extractor import Extractor
 from .lambda_ import Lambda, StarLambda
 
 
-class Map(Extractor):
+class MapBase(Extractor):
     def __init__(self, func, target=None, project=True, replace_error=None):
         super().__init__(target=target, project=project)
         self.func = func
+        self.lambda_replace_error = replace_error
 
         if isinstance(self.func, Extractor):
             self.lambda_ = self.func
         else:
-            self.lambda_ = Lambda(self.func, replace_error=replace_error)
+            self.lambda_ = self._get_lambda()
+
+    @property
+    def _get_lambda(self):
+        raise NotImplementedError()
 
     def extract(self, page, cache):
         x = page[self.target]
         assert isinstance(x, (Iterable,))
 
         return [self.lambda_({self.lambda_.target: e}) for e in x]
+
+
+class Map(MapBase):
+    def _get_lambda(self):
+        return Lambda(self.func, replace_error=self.lambda_replace_error)
 
     def __str__(self):
         return f'Map({self.func.__name__})'
 
 
-class StarMap(Extractor):
-    def __init__(self, func, target=None, project=True, replace_error=None):
-        super().__init__(target=target, project=project)
-        self.func = func
-
-        if isinstance(self.func, Extractor):
-            self.lambda_ = self.func
-        else:
-            self.lambda_ = StarLambda(self.func, replace_error=replace_error)
-
-    def extract(self, page, cache):
-        x = page[self.target]
-        assert isinstance(x, (Iterable,))
-
-        return [self.lambda_({self.lambda_.target: e}) for e in x]
+class StarMap(MapBase):
+    def _get_lambda(self):
+        return StarLambda(self.func, replace_error=self.lambda_replace_error)
 
     def __str__(self):
         return f'StarMap({self.func.__name__})'
