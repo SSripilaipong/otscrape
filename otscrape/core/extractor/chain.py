dade902783fdb3bdd9c881ade6bf87dc9549272e
SSripilaipong <santhapon.s@siametrics.com>
2020-11-29 18:41:02 +0700

    refactor: fix local class

--- otscrape/core/extractor/chain.py
+++ otscrape/core/extractor/chain.py
@@ -2,16 +2,23 @@ from typing import List
 from otscrape import Extractor
 
 
+class ChainedExtractor(Extractor):
+    def __init__(self, extractors, target=None, project=False):
+        super().__init__(target=target, project=project)
+
+        self.extractors = extractors
+
+    def extract(self, page, cache):
+        result = page[self.target]
+        for e in self.extractors:
+            result = {e.target: result}
+            result = e.extract(result, cache)
+        return result
+
+    def __str__(self):
+        return f'ChainedExtractor({self.extractors})'
+
+
 def chain(extractors: List[Extractor], target=None, project=True):
-    class ChainedExtractor(Extractor):
-        def extract(self, page, cache):
-            result = page[self.target]
-            for e in extractors:
-                result = {e.target: result}
-                result = e.extract(result, cache)
-            return result
 
-        def __str__(self):
-            return f'ChainedExtractor({extractors})'
-
-    return ChainedExtractor(target=target, project=project)
+    return ChainedExtractor(extractors, target=target, project=project)
