2086b1a2048eca24a911cd8ec75f9a65aeabca3a
SSripilaipong <santhapon.s@siametrics.com>
2020-12-02 00:52:35 +0700

    add url as a positional parameter

--- otscrape/core/page/page.py
+++ otscrape/core/page/page.py
@@ -6,3 +6,9 @@ from .util import get_page_meta
 
 class Page(PageBase, metaclass=get_page_meta(SimpleRequestLoader)):
     loader = None
+
+    def __init__(self, url=None, **kwargs):
+        kwargs = dict(kwargs)
+        kwargs['url'] = url
+
+        super().__init__(**kwargs)
