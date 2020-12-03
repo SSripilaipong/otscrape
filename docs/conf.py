73ad2ea3d20841cf5d81b37180a1f21c48e87480
SSripilaipong <santhapon.s@siametrics.com>
2020-12-03 20:45:16 +0700

    upload version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Santhapon Sripilaipong'
 author = 'Santhapon Sripilaipong'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
