f91db278ec94b957aae755ac6e393f5b41d0a544
SSripilaipong <santhapon.s@siametrics.com>
2020-12-03 13:37:06 +0700

    add __all__

--- otscrape/core/extractor/__init__.py
+++ otscrape/core/extractor/__init__.py
@@ -10,3 +10,7 @@ from .map import Map, StarMap, ChainMap
 from .lambda_ import Lambda, StarLambda
 from .soup import SoupFindAll, SoupSelect
 from .file import FileContent, FileName, FileLineNumber
+
+__all__ = ['Raw', 'RequestText', 'RequestStatusCode', 'RequestJSON', 'JSONDict', 'ETree', 'RegEx', 'TextSoup',
+           'JSON', 'DictPath', 'XPath', 'ZipDict', 'Chain', 'Map', 'StarMap', 'ChainMap', 'Lambda', 'StarLambda',
+           'SoupFindAll', 'SoupSelect', 'FileContent', 'FileName', 'FileLineNumber']
--- otscrape/core/page/__init__.py
+++ otscrape/core/page/__init__.py
@@ -1,3 +1,6 @@
 from .page import Page
 from .data import DataPage
 from .file import FileLinePage, JSONLinePage, CSVLinePage
+
+
+__all__ = ['Page', 'DataPage', 'FileLinePage', 'JSONLinePage', 'CSVLinePage']
